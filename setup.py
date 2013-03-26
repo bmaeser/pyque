2dcb2ef02214cd1758066a3ccbd4cb8c7aeb3296
Bernhard Mäser <bernhard.maeser@gmail.com>
2013-03-26 17:41:14 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import pyque
+from setuptools import setup, find_packages
+
+
+## dependencies 
+install_requires = [
+
+]
+
+packages = find_packages()
+
+setup(
+    name='pyque',
+    version=pyque.__version__,
+    author='Bernhard Maeser',
+    author_email='bernhard.maeser@gmail.com',
+    url='https://github.com/bmaeser/pyque',
+    license="MIT",
+    #scripts=['bin/dlvr'],
+    description="Backup and helper-utils for fabric",
+    long_description=open('README.rst').read(),
+    packages = packages,
+    include_package_data=True,
+    install_requires = install_requires,
+    zip_safe=False,
+    classifiers=(
+        'Development Status :: 2 - Pre-Alpha',
+        'Topic :: Utilities',
+        'Topic :: System :: Archiving :: Backup',
+        'Topic :: System :: Systems Administration',
+        'Operating System :: POSIX',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+    ),
+)
