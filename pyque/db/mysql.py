d9a4a1a928482ce07e4ba45e23ad0fdf78c21dc3
Bernhard Mäser <bernhard.maeser@gmail.com>
2013-04-03 18:20:33 +0200

    + mysqldump support

--- pyque/db/mysql.py
+++ pyque/db/mysql.py
@@ -1,12 +1,54 @@
 # -*- coding: utf-8 -*-
-from fabric.api import *
+
+import os
+from datetime import datetime
+
+from pyque.utils import sh
 
 
-def mysqldump(filename, dbname, user, password, port=5432,
-    tempdir='/tmp', mysqldump_path='mysqldump'):
-    """
+def mysqldump(filename, dbname, username=None, password=None, host=None,
+    port=None, tempdir='/tmp', mysqldump_path='mysqldump'):
+    """Perfoms a mysqldump backup.
     Create a database dump for the given database.
-
-
     """
-    return "asdf"
+
+    infodict = {}
+    anyerror = False
+
+    infodict['starttime'] = datetime.now()
+
+    filepath = os.path.join(tempdir, filename)
+
+    cmd = mysqldump_path
+    cmd += ' --result-file=' + os.path.join(tempdir, filename)
+
+    if username:
+        cmd += ' --user=%s' % username
+    if host:
+        cmd += ' --host=%s' % host
+    if port:
+        cmd += ' --port=%s' % port
+    if password:
+        cmd += ' --password=%s' % password
+
+    cmd += ' ' + dbname
+
+    ## run mysqldump
+    retcode, output = sh(cmd)
+
+    if retcode != 0: anyerror = True
+
+    infodict['cmd'] = cmd
+    infodict['endtime'] = datetime.now()
+    infodict['filepath'] = filepath
+
+    try:
+        infodict['filesize'] = os.path.getsize(filepath)
+    except OSError:
+        infodict['filesize'] = 0
+        anyerror = True
+        
+    infodict['error'] = anyerror
+    infodict['errortext'] = None if retcode == 0 else output
+
+    return infodict
