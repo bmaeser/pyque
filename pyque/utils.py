2ac6f044e9c1c5f768edd9ff4574206059d5ada7
Bernhard Mäser <bernhard.maeser@gmail.com>
2013-04-03 18:22:06 +0200

    + gzip support

--- pyque/utils.py
+++ pyque/utils.py
@@ -1,5 +1,7 @@
 # -*- encoding: utf-8 -*-
 
+import os
+from datetime import datetime
 from subprocess import Popen, PIPE, STDOUT
 
 
@@ -18,3 +20,37 @@ def sh(cmd):
 
     return (retcode, output)
 
+
+def gzip(filename):
+    infodict = {}
+    anyerror = False
+
+    infodict['starttime'] = datetime.now()
+
+    try:
+        infodict['original_filesize'] = os.path.getsize(filename)
+    except OSError:
+        infodict['original_filesize'] = 0
+        anyerror = True
+
+    ## run gzip
+    retcode, output = sh('gzip %s' % filename)
+    
+    if retcode != 0: anyerror = True
+
+    infodict['error'] = anyerror
+    infodict['errortext'] = None if retcode == 0 else output
+
+    new_filename = filename+'.gz'
+    
+
+    infodict['new_filename'] = new_filename
+    infodict['endtime'] = datetime.now()
+
+    try:
+        infodict['new_filesize'] = os.path.getsize(new_filename)
+    except OSError:
+        infodict['new_filesize'] = 0
+        anyerror = True
+
+    return infodict
