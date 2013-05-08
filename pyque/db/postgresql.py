7d35904cb95f1a57b634a935bc852713bec841fa
Bernhard Mäser <bernhard.maeser@gmail.com>
2013-05-08 18:11:54 +0200

    added db_list for postgresql

--- pyque/db/postgresql.py
+++ pyque/db/postgresql.py
@@ -3,6 +3,9 @@
 import os
 from datetime import datetime
 
+import psycopg2
+import psycopg2.extras
+
 from pyque.sh import sh
 
 def dump(filename, dbname, username=None, password=None, host=None,
@@ -48,3 +51,36 @@ def dump(filename, dbname, username=None, password=None, host=None,
     ## run pgdump
     return sh(cmd)
 
+
+def _cursor(username=None, password=None, host=None, port=None, db=None):
+    "returns a connected cursor to the database-server."
+
+    c_opts = {}
+
+    if username: c_opts['user'] = username
+    if password: c_opts['password'] = password
+    if host: c_opts['host'] = host
+    if port: c_opts['port'] = port
+
+    dbc = psycopg2.connect(**c_opts)
+    dbc.autocommit = True
+    cur = dbc.cursor()
+
+    return cur
+
+def db_list(username=None, password=None, host=None, port=None,
+        maintain_db='postgres'):
+    "returns a list of all databases on this server"
+
+    cur = _cursor(username=username, password=password, host=host, port=port,
+        db=maintain_db)
+
+    cur.execute('SELECT DATNAME from pg_database')
+    rows = cur.fetchall()
+
+    result = []
+    for row in rows:
+        result.append(row[0])
+
+    return result
+    
